40f69dd44ce61f7557267f050dd39c71aeee39a2
alihassnain-github <alimurtaza32721@gmail.com>
2024-11-10 08:55:27 -0800

    working on api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useCallback, useEffect, useState } from "react";
 import AmountInput from "./components/amount-input";
 import CurrencyInputs from "./components/currency-inputs";
 import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react";
 
 export default function App() {
 
   const [amount, setAmount] = useState("");
   const [toSelected, setToSelected] = useState("");
   const [fromSelected, setFromSelected] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState<null | number>(null);
+
+  const convertCurrency = useCallback(async () => {
+    setLoading(true)
+
+    const url = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_qajHydQHHmKNZam2RAgulZI10YdhQPbZXTUDm7OQ'
+    try {
+      const response = await fetch(`${url}&base_currency=${fromSelected}`)
+      const obj = await response.json()
+      const rate = obj.data[toSelected].value
+      setResult(rate * Number(amount.trim()))
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false)
+  }, [amount, fromSelected, toSelected])
 
 
   // Call the Api to show results
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (amount && fromSelected && toSelected) convertCurrency();
   }
+
+  useEffect(() => {
+    if (amount && fromSelected && toSelected) convertCurrency();
+  }, [amount, toSelected, fromSelected])
 
 
   return (
@@ -31,10 +55,23 @@ export default function App() {
             fromSelected={fromSelected}
             setFromSelected={setFromSelected}
           />
-          <Button type="submit" className="my-4">
-            Convert
+          <Button disabled={loading} type="submit" className="my-4">
+            {loading ? (
+              <>
+                <Loader2 className="animate-spin" />
+                Please wait
+              </>
+            ) : "Convert"}
           </Button>
         </form>
+        {
+          result && (
+            <div className="mt-2">
+              <span className="font-semibold text-gray-400">{amount} {fromSelected} =</span>
+              <h4 className="text-xl font-semibold mt-2">{result} {toSelected}</h4>
+            </div>
+          )
+        }
       </div>
     </main>
   )
